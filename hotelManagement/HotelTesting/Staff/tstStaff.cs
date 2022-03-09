80142c059192f31cdf1938b93f606d38c2359d4c
Hassaan Muhammad <P2645492@my365.dmu.ac.uk>
2022-03-09 03:23:35 +0000

    DobOK

--- hotelManagement/HotelTesting/Staff/tstStaff.cs
+++ hotelManagement/HotelTesting/Staff/tstStaff.cs
@@ -16,5 +16,97 @@ namespace HotelTesting
             Assert.IsNotNull(AStaff);
 
         }
+
+
+        [TestMethod]
+        public void AddressOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //test to see that it exists
+            String TestData = "21";
+            //assign the data to the property
+            AStaff.Address = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.Address, TestData);
+
+        }
+
+
+
+
+        [TestMethod]
+        public void DobOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //test to see that it exists
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AStaff.DobOK = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.DobOK, TestData);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
